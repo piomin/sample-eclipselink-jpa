2bb176ca537d9bda1a8d559f17095099abf53fc9
piomin <piotr.minkowski@gmail.com>
2026-01-10 00:12:27 +0100

    Update dependency org.springdoc:springdoc-openapi-starter-webmvc-ui to v3

--- src/main/java/pl/piomin/services/jpa/JpaApplication.java
+++ src/main/java/pl/piomin/services/jpa/JpaApplication.java
@@ -5,31 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-//@EnableSwagger2
 public class JpaApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(JpaApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
-
-//    @Bean
-//    public Docket swaggerApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//            .select()
-//                .apis(RequestHandlerSelectors.basePackage("pl.piomin.services.jpa"))
-//                .paths(any())
-//            .build()
-//            .apiInfo(new ApiInfoBuilder().version("1.0").title("JPA API").description("Documentation JPA API v1.0").build());
-//    }
-
-//    @Bean
-//    public DataSource dataSource() {
-//        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-//        dataSource.setUrl("jdbc:oracle:thin:@localhost:1521/eclipselink");
-//        dataSource.setUsername("piomin");
-//        dataSource.setPassword("Piot_123");
-//        return dataSource;
-//    }
 
 }
